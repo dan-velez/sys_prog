d5e8bd1c145e25313d8cd08c6865f3041db69de6
Danny <daniel.enr.velez@gmail.com>
2021-07-22 12:32:21 -0400

    Initial

---
+++ prog.c
@@ -0,0 +1,11 @@
+/* ~/projects/sys_prog/
+ * Help stay focused on development.
+ * Create repo for C code. Add books to repo.
+ * Compiler: x86_64-w64-mingw32-gcc.exe 
+ */
+
+
+int main(int argc, char** argv)
+{
+    int i = 0;
+}
