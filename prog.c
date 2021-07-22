5a000ea4f0ad31ef8faf57eabd552d57e6cdbdb1
Danny <daniel.enr.velez@gmail.com>
2021-07-22 16:16:54 -0400

    Add notes

--- prog.c
+++ prog.c
@@ -1,11 +1,26 @@
 /* ~/projects/sys_prog/
  * Help stay focused on development.
  * Create repo for C code. Add books to repo.
- * Compiler: x86_64-w64-mingw32-gcc.exe 
+ * Compiler: x86_64-w64-mingw32-gcc.exe
+ * Bash: C:\cygwin64\Cygwin.bat 
+ * How is data passed on the stack?
+ * Memory management.
+ * More on stacks, buffer, stack smashing, etc...
+ * Time and Space Complexity
+ * Dynamic programming.
  */
 
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
     int i = 0;
+    for(i = 0; i < 10; i++) {
+        printf("%d\n", i);
+    }
+}
+
+void swap(float f[], int i, int j)
+{
+
 }
