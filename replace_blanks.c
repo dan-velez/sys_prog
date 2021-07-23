83ed12d0af9b0dbe89542a4d5ad762df21a3856a
Danny <daniel.enr.velez@gmail.com>
2021-07-23 16:14:42 -0400

    Work through K&R book

---
+++ replace_blanks.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* Copy input to output, replacing multi blanks with single blanks. */
+
+void replace_blanks()
+{
+    int c;
+    int bc;
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\t' || c == '\n' || c == ' ') {
+            if (bc < 1) putchar(c);
+            ++bc;
+        } else {
+            putchar(c);
+            bc = 0;
+        }
+    }
+}
+
+int main()
+{
+    replace_blanks();
+}
