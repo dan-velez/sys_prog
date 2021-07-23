c016682d16378f6b3ef5d120b3d206b10f7c1099
Danny <daniel.enr.velez@gmail.com>
2021-07-23 14:20:37 -0400

    Work through K&R book

--- farenheit.c
+++ farenheit.c
@@ -6,16 +6,15 @@ int test_types()
     double y;
     short z;
     char a;
-    char* a;
-
+    char* b;
 }
 
 int main()
 {
     /* Print farenheit-celsius table. */
 
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0;
     upper = 300;
@@ -24,8 +23,19 @@ int main()
     fahr = lower;
 
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
+
+    /* Integer arithmic is truncated to zero. */
+    int z = 5 / 9; 
+    float y = 5.0 / 9.0;
+    printf("%d %f", z, y);
+
+    int a = 5;
+    float b = 9.0;
+
+    float c = a / b;
+    int d = a / b;
 }
